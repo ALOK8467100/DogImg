8d44d1d5ed236be89e6266302625d1f85c83d09e
alok <60954798+ALOK8467100@users.noreply.github.com>
2024-10-25 19:42:41 +0530

    Add files via upload

---
+++ Dog_projects/index.js
@@ -0,0 +1,20 @@
+// const images = document.querySelector('img');
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', () => {
+    // fetch('https://dog.ceo/api/breeds/images/random')
+    //   .then(response => response.json())
+    //   .then((json) => {
+    //     console.log(json.message)
+    //     // image.src = json.message;
+    // })
+  // })
+
+
+
+  // fetch('https://dog.ceo/api/breeds/image/random')
+  //     .then(response => response.json())
+  //     .then((json) => {
+  //       console.log(json.message)
+  //       images.src = json.message;
+  //   })
